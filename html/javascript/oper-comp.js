c966f1fb65decc531ad685c2191697b93fb4d97d
gmonferrer <105653474+gmonferrer@users.noreply.github.com>
2023-03-15 18:31:08 +0100

    15

--- html/javascript/oper-comp.js
+++ html/javascript/oper-comp.js
@@ -1,4 +1,4 @@
 var x=7, y=4;
 document.write (x+y);
-document.write("<p>Resultado de x==y? " + (x!=y));
+document.write("<p>Resultado de x!=y? " + (x!=y));
 document.getElementById("resultado").innerHTML = "El resultado es " + (x!=y);
