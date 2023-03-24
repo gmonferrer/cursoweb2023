fc53252c4a4330521a19bfaa33641cb70bf4e594
gmonferrer <105653474+gmonferrer@users.noreply.github.com>
2023-03-24 17:47:29 +0100

    24

---
+++ html/javascript/funciones1.js
@@ -0,0 +1,12 @@
+function funcion() {
+    document.getElementById("demo").innerHTML = "¡Hola!";
+    }
+
+function funcArg(nombre,trabajo) {
+    document.getElementById("demo1").innerHTML =    
+        "Aquí tenemos a " + nombre + ", es " + trabajo + ".";}
+
+function myfunction(txt) {
+document.getElementById("demo2").innerHTML = txt         
+}
+
