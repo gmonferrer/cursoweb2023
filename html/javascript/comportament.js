734f31ce6aef2a647dd91a98e62d3e5d446a64b4
gmonferrer <105653474+gmonferrer@users.noreply.github.com>
2023-04-12 15:49:07 +0200

    examen

--- html/javascript/comportament.js
+++ html/javascript/comportament.js
@@ -1,7 +1,7 @@
         function agranda() {document.getElementById("paragraf").style="font-size:35px";
         }
-        function rojo() {document.getElementById("titol").style="color:red";
+        function vermell() {document.getElementById("titol").style="color:red";
         }
-        function contenido() {document.getElementById("canvia").innerHTML="Exercici 5 resolt";
+        function contingut() {document.getElementById("canvia").innerHTML="Exercici 5 resolt";
 
         }
